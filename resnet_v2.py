6073e0c996bd2a34fff09ee42746b33763cda9eb
fcu-D0441406 <ian061504@gmail.com>
2019-06-02 12:54:54 +0800

    Add files via upload

--- resnet_v2.py
+++ resnet_v2.py
@@ -1,148 +1,91 @@
 import tensorflow as tf
 import numpy as np
-import os
 
-'''
-self.net4 = self.block(self.net3,512,2,is_training=is_training,scope='block4')
-print(self.net4)#/16
-'''
+bk = [2,2,2,2]
 
-class ResNet():#resnet34(3,4,6,3) resnet18(2,2,2,2)
-    def __init__(self,class_num=10,is_training=True,scope='resnet'):
-        img_size = 64
+class Resnet_v2:
+    
+    def __init__(self,img_size,channel,class_num,batch_size=64,trainable=True):
+        self.img_size = img_size
+        self.channel = channel
         self.class_num = class_num
-        with tf.variable_scope(scope):
-            self.x = tf.placeholder(tf.float32,[None,img_size,img_size,3])/255.0
-            net = tf.layers.conv2d(self.x,64,5,2,padding='SAME')
-            net = tf.nn.relu(tf.layers.batch_normalization(net,training=is_training))
-            net = tf.layers.max_pooling2d(net,3,2,padding='SAME')
-            print(net)
-            
-            self.net1 = self.block(net,64,2,is_training=is_training,scope='block1')
-            print(self.net1)#/2
-            
-            self.net2 = self.block(self.net1,128,2,is_training=is_training,scope='block2')
-            print(self.net2)#/4
-            
-            self.net3 = self.block(self.net2,256,2,is_training=is_training,scope='block3')
-            print(self.net3)#/8
-            
-            self.pooling = tf.layers.average_pooling2d(self.net3,[4,4],1)
-            print(self.pooling)
-            net = tf.layers.flatten(self.pooling,'flatten_layer')
-            self.prediction = tf.layers.dense(net,class_num)
-            #self.prediction = tf.nn.softmax(net)
-            print(self.prediction)
-            self.unsample()
+        self.batch_size = batch_size
+        self.x = tf.placeholder(tf.float32,[self.batch_size,self.img_size,self.img_size,self.channel])
+        self.weight_init = tf.contrib.layers.variance_scaling_initializer()
+        self.build_net(trainable)
+ 
         
-    def FPN_net(self):
-        self.pre1 = tf.add(tf.layers.conv2d_transpose(self.net3,256,3,2,padding='SAME'),
-                           tf.layers.conv2d(self.net2,256,1,1))
-        self.pre1 = tf.layers.conv2d(self.pre1,256,3,1,padding='SAME')
-        self.pre2 = tf.add(tf.layers.conv2d_transpose(self.pre1,256,3,2,padding='SAME'),
-                           tf.layers.conv2d(self.net1,256,1,1))
-        self.pre2 = tf.layers.conv2d(self.pre2,256,3,1,padding='SAME')
     
-    
-    
-    def block(self,x, n_out, n, is_training=True, scope="block"):
-        with tf.variable_scope(scope):
-            for i in range(n):
-                scope_name = scope+'_'+str(i)
-                x = self.bottleneck(x,n_out,is_training=is_training,scope=scope_name)
-                #print(x)
-        return x
-    
-    def bottleneck(self,x, n_out, is_training=True, scope="bottleneck"):
-        if(x.get_shape()[-1]==n_out):
-            stride = 1
-        else:
-            stride = 2
-        with tf.variable_scope(scope):
-            net = tf.nn.relu(tf.layers.batch_normalization(x,training=is_training))
-            net = tf.layers.conv2d(net,n_out,3,stride,padding='SAME')
-            net = tf.nn.relu(tf.layers.batch_normalization(net,training=is_training))
-            net = tf.layers.conv2d(net,n_out,3,1,padding='SAME')
-
-            if (x.get_shape()[-1]==n_out):
-                shortcut = x
-            else:
-                shortcut = tf.layers.conv2d(x,n_out,1,2,padding='SAME')
-            return tf.add(net,shortcut)
-    
-    def unsample(self,scope='upresnet'):
-        img_size = 64
-        with tf.variable_scope(scope):
-            self.unconv1 = tf.layers.conv2d_transpose(self.pooling,256,4,1)
-            print(self.unconv1)
-            self.unconv2 = tf.layers.conv2d_transpose(self.unconv1,128,3,2,padding='SAME')
-            print(self.unconv2)
-            self.unconv3 = tf.layers.conv2d_transpose(self.unconv2,64,3,2,padding='SAME')
-            print(self.unconv3)
-            self.unconv4 = tf.layers.conv2d_transpose(self.unconv3,64,3,2,padding='SAME')
-            print(self.unconv4)
-            self.unconv5 = tf.layers.conv2d_transpose(self.unconv4,self.class_num,3,2,padding='SAME')
-            #self.unconv5 = tf.reshape(self.unconv5,[-1,img_size*img_size*2])
-            print(self.unconv5)
-    
-    
-class ResNet_50():
-    def __init__(self,class_num=10,is_training=True,scope='resnet'):
-        img_size = 64
-        with tf.variable_scope(scope):
-            self.x = tf.placeholder(tf.float32,[None,img_size,img_size,3])
-            net = tf.layers.conv2d(self.x,64,3,2,padding='SAME')
-            net = tf.nn.relu(tf.layers.batch_normalization(net,training=is_training))
-            net = tf.layers.max_pooling2d(net,3,2,padding='SAME')
+    def build_net(self,trainable):
+        with tf.variable_scope('resnet_v2'):
+            #tf.contrib.layers.variance_scaling_initializer()
+            net = tf.layers.conv2d(self.x,64,7,2,padding='SAME',
+                                      kernel_initializer=tf.contrib.layers.variance_scaling_initializer(),
+                                      kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-4))
+                                
+            net = tf.layers.batch_normalization(net,training=trainable)
+            net = tf.nn.relu(net)
+            net = tf.layers.max_pooling2d(net,3,2,padding='SAME')#128
             print(net)
-            net = self.block(net,64,2,is_training=is_training,scope='block1')
-            print(net)
-            net = self.block(net,128,2,is_training=is_training,scope='block2')
-            print(net)
-            net = self.block(net,256,2,is_training=is_training,scope='block3')
-            print(net)
-            '''
-            net = self.block(net,512,3,is_training=is_training,scope='block4')
-            print(net)
-            '''
-            net = tf.layers.average_pooling2d(net,[4,4],1)
-            net = tf.layers.flatten(net,'flatten_layer')
-            self.prediction = tf.layers.dense(net,class_num)
-            print(self.prediction)
-            #self.prediction = tf.nn.softmax(net)
+            resnet1 = self.res_block(net,64,1,bk[0],trainable,'res_block0')
+            print(resnet1)
             
-    def block(self,x, n_out, n, is_training=True, scope="block"):
-        with tf.variable_scope(scope):
-            for i in range(n):
-                scope_name = scope+'_'+str(i)
-                x = self.bottleneck(x,n_out,i,is_training=is_training,scope=scope_name)
-                    
-        return x
+            resnet2 = self.res_block(resnet1,128,2,bk[1],trainable,'res_block1')
+            print(resnet2)
+            
+            resnet3 = self.res_block(resnet2,256,2,bk[2],trainable,'res_block2')
+            print(resnet3)
+            
+            resnet4 = self.res_block(resnet3,512,2,bk[3],trainable,'res_block3')
+            print(resnet4)
+            
+            avg_pool = tf.layers.average_pooling2d(resnet4,7,1,'VALID')
+            flat = tf.layers.flatten(avg_pool)
+            self.prediction = tf.layers.dense(flat,self.class_num)
     
-    def bottleneck(self,x, n_out,time,is_training=True, scope="bottleneck"):
-        if(time==0 and x.get_shape()[-1]!=n_out):
-            stride = 2
+    def res_block(self,x,ch,stride,b_num,trainable,scope_name=''):
+        with tf.variable_scope(scope_name):
+            for i in range(b_num):
+                if(i==0):
+                    x = self.bottleneck(x,ch,stride,trainable)
+                else:
+                    x = self.bottleneck(x,ch,1,trainable)
+            
+            return x
+    
+    def bottleneck(self,x,ch,stride,trainable):
+        pre_act = tf.layers.batch_normalization(x,training=trainable)
+        pre_act = tf.nn.relu(pre_act)
+        
+        if(stride==2 or ch==x.shape[-1]):
+            short_cut = tf.layers.conv2d(pre_act,ch*4,1,stride,padding='SAME')
+            
         else:
-            stride = 1
-        with tf.variable_scope(scope):
-            net = tf.nn.relu(tf.layers.batch_normalization(x,training=is_training))
-            net = tf.layers.conv2d(net,n_out,1,stride,padding='SAME')
-            net = tf.nn.relu(tf.layers.batch_normalization(net,training=is_training))
-            net = tf.layers.conv2d(net,n_out,3,1,padding='SAME')
-            net = tf.nn.relu(tf.layers.batch_normalization(net,training=is_training))
-            net = tf.layers.conv2d(net,n_out*4,1,1,padding='SAME')
+            short_cut = x
+        
+        x2 = tf.layers.conv2d(x,ch,1,stride,padding='SAME',
+                              kernel_initializer=tf.contrib.layers.variance_scaling_initializer(),
+                              kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-4))
+        x2 = tf.layers.batch_normalization(x2,training=trainable)
+        x2 = tf.nn.relu(x2) 
+        
+        x2 = tf.layers.conv2d(x2,ch*4,3,1,padding='SAME',
+                              kernel_initializer=tf.contrib.layers.variance_scaling_initializer(),
+                              kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-4))
+        x2 = tf.layers.batch_normalization(x2,training=trainable)
+        x2 = tf.nn.relu(x2) 
+        
+        x2 = tf.layers.conv2d(x2,ch*4,1,1,padding='SAME',
+                              kernel_initializer=tf.contrib.layers.variance_scaling_initializer(),
+                              kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-4))
+        x2 = tf.layers.batch_normalization(x2,training=trainable)
+        x2 = tf.nn.relu(x2) 
+        #print(short_cut,x2)
+        return tf.add(x2,short_cut)
+        
 
-            if(x.get_shape()[-1]==n_out):
-                shortcut = tf.layers.conv2d(x,n_out*4,1,1,padding='SAME')
-            elif(time==0):
-                shortcut = tf.layers.conv2d(x,n_out*4,1,2,padding='SAME')
-            else:
-                shortcut = tf.layers.conv2d(x,n_out*4,1,1,padding='SAME')
-            return tf.add(net,shortcut)
-'''
-if(__name__=='__main__'):
-    x = tf.random_normal([32, 64, 64, 1])
-    resnet = ResNet_50(x)
-    print(resnet.prediction)
-'''
-
+r = Resnet_v2(224,3,10)
+        
+        
+        
+        
